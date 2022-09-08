62ef37d793dc15eb246b7a97d04873f27b68b6a7
turan çabuk <turan6677@gmail.com>
2022-09-08 19:56:57 +0300

    CryptoCurrency model added.

---
+++ cryptoCurrencyProjectMVVM/Model/CrypoCurrency.swift
@@ -0,0 +1,18 @@
+//
+//  CrypoCurrency.swift
+//  cryptoCurrencyProjectMVVM
+//
+//  Created by Turan Çabuk on 7.09.2022.
+//
+
+import Foundation
+
+
+struct CryptoCurrency {
+    
+struct CryptoCurrency : Decodable {
+        
+        var currency : String
+        var price : String
+    }
+}
